3e33784b68ce2c3b530a04acafd082a789b26317
Amir Mohsen <admin@mihanmail.com>
2025-02-27 22:07:05 +0100

    fix: add str_starts_with and str_ends_with polyfills for PHP 7.4
    - Add polyfill for str_starts_with function - Add polyfill for str_ends_with function for future use - All polyfills use mb_* functions for proper UTF-8 support

--- src/Polyfill.php
+++ src/Polyfill.php
@@ -13,4 +13,30 @@ if (!function_exists('str_contains')) {
     function str_contains(string $haystack, string $needle): bool {
         return $needle !== '' && mb_strpos($haystack, $needle) !== false;
     }
+}
+
+if (!function_exists('str_starts_with')) {
+    /**
+     * Polyfill for str_starts_with() function added in PHP 8.0
+     *
+     * @param string $haystack The string to search in
+     * @param string $needle The substring to search for
+     * @return bool Returns true if haystack starts with needle, false otherwise
+     */
+    function str_starts_with(string $haystack, string $needle): bool {
+        return $needle !== '' && mb_strpos($haystack, $needle) === 0;
+    }
+}
+
+if (!function_exists('str_ends_with')) {
+    /**
+     * Polyfill for str_ends_with() function added in PHP 8.0
+     *
+     * @param string $haystack The string to search in
+     * @param string $needle The substring to search for
+     * @return bool Returns true if haystack ends with needle, false otherwise
+     */
+    function str_ends_with(string $haystack, string $needle): bool {
+        return $needle !== '' && mb_substr($haystack, -mb_strlen($needle)) === $needle;
+    }
 } 
